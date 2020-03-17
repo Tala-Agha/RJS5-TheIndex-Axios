e9a16770b6360b1fdaec9f1ac5f7c0752ce6849d
Tala Agha <macbook@MacBooks-MacBook-Pro.local>
2020-03-17 08:52:37 +0200

    React T5 lifecycle

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
-
-import authors from "./data.js";
+import axios from "axios";
 
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
 import AuthorDetail from "./AuthorDetail";
+import Loading from "./Loading";
 
 class App extends Component {
   state = {
-    currentAuthor: null
+    currentAuthor: null,
+    authors: [],
+    loading: true
   };
 
-  selectAuthor = author => this.setState({ currentAuthor: author });
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://the-index-api.herokuapp.com/api/authors/"
+      );
+      const authors = response.data;
+      this.setState({ authors, loading: false });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  selectAuthor = async author => {
+    try {
+      this.setState({ loading: true });
+      const response = await axios.get(
+        `https://the-index-api.herokuapp.com/api/authors/${author.id}/`
+      );
+      this.setState({ currentAuthor: response.data, loading: false });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   unselectAuthor = () => this.setState({ currentAuthor: null });
 
@@ -20,7 +44,14 @@ class App extends Component {
     if (this.state.currentAuthor) {
       return <AuthorDetail author={this.state.currentAuthor} />;
     } else {
-      return <AuthorList authors={authors} selectAuthor={this.selectAuthor} />;
+      return this.state.loading ? (
+        <Loading />
+      ) : (
+        <AuthorList
+          authors={this.state.authors}
+          selectAuthor={this.selectAuthor}
+        />
+      );
     }
   };
 
